66cd7fd46689ccff69450b899ca1493ba3b2fdcf
Мария Самойлова <maria@MacBook-Air-Maria1w.local>
2023-01-06 01:32:51 +0300

    feat: cart refresh

--- src/modules/controller/refresher.ts
+++ src/modules/controller/refresher.ts
@@ -11,6 +11,16 @@ class Refresher {
       total.innerText = ` ${Cart.getTotal()} BYN`;
     }
   }
+  static refreshCart(): void {
+    const amount: HTMLElement | null = document.querySelector('.summary_products');
+    if (amount) {
+      amount.innerText = `Total products: ${Cart.getAmount()} `;
+    }
+    const total: HTMLElement | null = document.querySelector('.summary_price');
+    if (total) {
+      total.innerText = `Total price: ${Cart.getTotal()} BYN`;
+    }
+  }
 }
 
 export default Refresher;
